b2acf374bef000bf1713202b68785afb8e4552f3
João Vitor Andrade Miranda <vitorandrademiranda@hotmail.com>
2022-06-02 22:29:08 -0400

    Version 2.0

---
+++ blog-service.js
@@ -0,0 +1,62 @@
+/*********************************************************************************************
+* WEB322 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: João Vitor Andrade Miranda Student ID: 116499203 Date: 05/27/2022
+*
+* Online (Heroku) URL:
+*
+**********************************************************************************************/
+module.exports = {getCategories,getPublishedPosts,getAllPosts,initialize} 
+const fs = require("fs"); // required at the top of your module
+const { resolve } = require("path");
+var categ = require("./data/categories.json")
+var getPost = require('./data/posts.json')
+posts= []
+publishedPosts = []
+categories = []
+
+
+function initialize(){
+    fs.readFile('./data/posts.json', 'utf8', (err, readData) => { if (err) reject(err);posts = JSON.parse(readData);resolve();});
+    fs.readFile('./data/categories.json', 'utf8', (err, readData) => { if (err) reject(err);categories = JSON.parse(readData);resolve();});
+}
+
+function getAllPosts () {
+    return new Promise(function(resolve,reject){
+        if(getPost.length!=0){
+            resolve(getPost)
+        }else{
+            reject("The JSON is empty and no result to return")
+        }
+    })
+}
+
+function getPublishedPosts () {
+    return new Promise(function(resolve,reject){
+        if(getPost.length!=0){
+            var store = 0;
+            for(let i =0; i<getPost.length;i++){
+                if(getPost[i].published){
+                    publishedPosts[store] = getPost[i]
+                    store++
+                }
+            }
+            resolve(publishedPosts)
+        }else{
+            reject("The JSON is empty and no result to return")
+        }
+    })
+}
+
+function getCategories(){
+    return new Promise(function(resolve,reject){
+        if(categ.length!=0){
+            resolve(categ)
+        }else{
+            reject("The JSON is empty and no result to return")
+        }
+    })
+}
---
+++ server.js
@@ -0,0 +1,64 @@
+/*********************************************************************************************
+* WEB322 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: João Vitor Andrade Miranda Student ID: 116499203 Date: 05/27/2022
+*
+* Online (Heroku) URL:
+*
+**********************************************************************************************/
+
+var HTTP_PORT = process.env.PORT || 8080;
+const blog_service = require('./blog-service');
+//const categ = require("./data/categories.json");
+//const post = require("./data/posts.json");
+//var publishedPosts = []
+var express = require("express");
+const res = require('express/lib/response');
+var app = express();
+
+
+//css
+//app.use(express.static("static"));
+app.use(express.static('public'));
+app.use(express.static('views'));
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    console.log('Express http server listening on port',HTTP_PORT)
+    res.redirect('about')
+});
+
+app.get("/about", (req, res) => {
+    res.redirect("about.html")
+});
+
+app.get("/blog", (req, res) => {
+    blog_service.getPublishedPosts().then((getResponse)=>{res.send(getResponse)}).catch((getReject)=>{res.send(getReject)})
+});
+
+app.get("/posts", (req, res) => {
+    //var jsonString = JSON.stringify(post);
+    //res.send(jsonString);
+    //res.json(post);
+    blog_service.getAllPosts().then((getResponse)=>{res.send(getResponse)}).catch((getReject)=>{res.send(getReject)})
+
+});
+app.get("/categories", (req, res) => {
+    //var jsonString = JSON.stringify(categ);
+    //res.send(jsonString);
+    //res.json(categ);
+    blog_service.getCategories().then((getResponse)=>{res.send(getResponse)}).catch((getReject)=>{res.send(getReject)})
+});
+
+app.get("*", (req, res) => {
+    console.log("The specified route does not exist")
+    res.send("<h1><b>ERROR 404 - Page does not exist<b><h1>")
+});
+
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT)
+//blog_service.initialize().then(()=>{app.listen(HTTP_PORT)}).catch(()=>{"Fail to initialize the data."})
